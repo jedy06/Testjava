24d578ae599d31db21ca94474ba2f554d96299eb
jedy06 <jabm06@homail.com>
2024-05-13 18:30:46 -0400

    Correccion de nombre

--- src/main/java/com/api/test/user/h2/TestRGaApplication.java
+++ src/main/java/com/api/test/user/h2/TestRGaApplication.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.api.bci.user.h2")
-public class TestBciSermalucApplication {
+@ComponentScan(basePackages = "com.api.test.user.h2")
+public class TestRGaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestBciSermalucApplication.class, args);
+		SpringApplication.run(TestRGaApplication.class, args);
 	}
 
 }
